00d15d27b192d3c2dbf62e99c50af4008861d919
Wilson Oliveira <wilso.mg.oliveira@gmail.com>
2025-02-14 15:19:18 -0300

    Atualização

--- Python3_M1/ex012_desconto_a_vista.py
+++ Python3_M1/ex012_desconto_a_vista.py
@@ -1,11 +1,8 @@
-# Entrada do usuário
+# Solicita o preço do produto
+preco_produto = float(input('Digite o preço do produto (Em R$): '))
 
-preco = float(input('Digite o preço do produto: '))
+# Calcula o valor do desconto de 5%
+valor_desconto = preco_produto * 5 / 100
 
-#calculo do desconto
-
-novo = preco - (preco * 5/ 100) 
-
-# impressão dos resultados
-
-print(f'O produto que custava R${preco}, na promoção com 5% de desconto, vai custar R${novo:.2f}R$.') 
+# Exibe o preço original e o preço com o desconto aplicado
+print(f'O produto de preço R${preco_produto:.2f}, com 5% de desconto, tem o novo preço de R${preco_produto - valor_desconto:.2f}')
