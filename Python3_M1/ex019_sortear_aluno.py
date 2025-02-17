bf5f0fbb8079ee233fdfddc6f09fe2dbc3440784
Wilson Oliveira <wilso.mg.oliveira@gmail.com>
2025-02-17 10:58:16 -0300

    Atualização

--- Python3_M1/ex019_sortear_aluno.py
+++ Python3_M1/ex019_sortear_aluno.py
@@ -1,8 +1,16 @@
 from random import choice
-n1 = str(input('Primeiro aluno: '))
-n2 = str(input('Segundo aluno: '))
-n3 = str(input('Terceiro aluno: '))
-n4 = str(input('Quarto aluno: '))
-lista = [n1, n2, n3, n4]
+
+# Recebendo os nomes dos alunos
+aluno_1 = input('Digite o nome do primeiro aluno: ')
+aluno_2 = input('Digite o nome do segundo aluno: ')
+aluno_3 = input('Digite o nome do terceiro aluno: ')
+aluno_4 = input('Digite o nome do quarto aluno: ')
+
+# Criando a lista de alunos
+lista = [aluno_1, aluno_2, aluno_3, aluno_4]
+
+# Escolhendo aleatoriamente um aluno
 escolhido = choice(lista)
-print(f'O aluno escolhido foi {escolhido}')
+
+# Exibindo o nome do aluno escolhido
+print(f'Quem vai limpar o quadro é {escolhido}')
